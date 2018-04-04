4231fa0e40f71c13e0b6baffa600e0d3286dc862
Felix Peither <felix@peither.de>
2018-04-04 12:45:11 +0200

    .class files im ornder

--- SA/test/edu/hm/software_architektur/a02_staticanalyzer/MyComplexityAnalyzerTest.java
+++ SA/test/edu/hm/software_architektur/a02_staticanalyzer/MyComplexityAnalyzerTest.java
@@ -10,8 +10,8 @@ import org.junit.Test;
  */
 public class MyComplexityAnalyzerTest {
     
-   // public final Path path = Paths.get("C:\\Users\\Computer\\Documents\\NetBeansProjects\\SA\\SA\\build\\classes\\edu\\hm\\software_architektur");
-    public final Path path = Paths.get("C:\\Users\\Edo\\Documents\\NetBeansProjects\\SA\\SA\\test\\edu\\hm\\software_architektur\\a02_staticanalyzer");
+    public final Path path = Paths.get("C:\\Users\\Computer\\Documents\\NetBeansProjects\\SA\\SA\\build\\test\\classes\\edu\\hm\\software_architektur\\a02_staticanalyzer");
+//    public final Path path = Paths.get("C:\\Users\\Edo\\Documents\\NetBeansProjects\\SA\\SA\\test\\edu\\hm\\software_architektur\\a02_staticanalyzer");
         
     
     public MyComplexityAnalyzerTest() {
@@ -19,7 +19,7 @@ public class MyComplexityAnalyzerTest {
 
     @Test(timeout = 10000)
     public void test1() throws Exception {
-        MyComplexityAnalyzer analyzer = new MyComplexityAnalyzer(Paths.get(path.toAbsolutePath().toString()+"ABC=2"));
+        MyComplexityAnalyzer analyzer = new MyComplexityAnalyzer(Paths.get(path.toAbsolutePath().toString()));
         System.out.println(analyzer.analyzeClassfiles().toString());
         
     }
