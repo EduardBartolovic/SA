a5a0e367af4f34eded1649d4a6e63a082e20d94e
Felix Peither <felix@peither.de>
2018-04-12 21:30:46 +0200

    instableParamerters and 1. game rule

---
+++ SA/src/edu/hm/software_architektur/a03_undercut/gamerules/ChoiceDifferenceGameRule.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hm.software_architektur.a03_undercut.gamerules;
+
+/**
+ *
+ * @author Computer
+ */
+public class ChoiceDifferenceGameRule implements GameRule{
+
+    @Override
+    public int[] calculateScore(int playerAChoice, int playerBChoice) {
+        final int[] scores = new int[2]; // int[0] score of player A, int[1] score of player B
+        final int higherScore = Math.max(playerBChoice, playerAChoice);
+        final int lowerScore = Math.max(playerAChoice, playerBChoice);
+        
+        if(playerAChoice == playerBChoice - 1) {
+            scores[0] += playerAChoice + playerBChoice;
+        } else if(playerBChoice == playerAChoice - 1) {
+            scores[1] += playerAChoice + playerBChoice;
+        } else if (Math.abs(playerAChoice - playerBChoice) == 2) {
+            scores[0] += playerAChoice;
+            scores[1] += playerBChoice;
+        } else if (playerAChoice - playerBChoice > 2) {
+            scores[0] += playerAChoice + playerBChoice;
+        } else if (playerBChoice - playerAChoice > 2) {
+            scores[1] += playerAChoice + playerBChoice;
+        }
+        return scores;
+    }
+    
+}
---
+++ SA/src/edu/hm/software_architektur/a03_undercut/parameter/InstableParameters.java
@@ -0,0 +1,58 @@
+package edu.hm.software_architektur.a03_undercut.parameter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Edo
+ */
+public class InstableParameters implements Parameters{
+    /**
+     * score to win
+     */
+    public static final int SCORETOWIN = 42;
+    
+    /**
+     * the first choose range 1,2,3,4
+     */
+    private final List<Integer> chooseRangeOne = Arrays.asList(1,2,3,4);//new ArrayList<>();
+    
+    /**
+     * the secont choose range 2,3,4,5
+     */
+    private final List<Integer> chooseRangeTwo = Arrays.asList(2,3,4,5);
+    
+    /**
+     * the third choose range 1/3/5
+     */
+    private final List<Integer> chooseRangeThree = Arrays.asList(1,3,5);
+    
+    /**
+     * for keeping track of how many turns there were
+     */
+    private int turnCount = 0;
+    
+    @Override
+    public int getScoreToWin() {
+        return SCORETOWIN;
+    }
+
+    @Override
+    public List<Integer> getChooseRange() {
+        List<Integer> retChooseRange = new ArrayList<>();
+        if (turnCount%3==0) {
+            retChooseRange = chooseRangeThree;
+        } else if (turnCount%2==0) {
+            retChooseRange = chooseRangeTwo;
+        } else {
+            retChooseRange = chooseRangeOne;
+        }
+        turnCount++;
+        return retChooseRange;
+    }
+
+    
+}
