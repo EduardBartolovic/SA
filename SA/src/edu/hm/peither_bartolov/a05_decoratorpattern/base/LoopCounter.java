d9667bbf0293d88c707504618265e52fd3df179b
EduardBartolovic <zxeduardxz@gmail.com>
2018-04-24 14:22:26 +0200

    tests

--- SA/src/edu/hm/peither_bartolov/a05_decoratorpattern/base/LoopCounter.java
+++ SA/src/edu/hm/peither_bartolov/a05_decoratorpattern/base/LoopCounter.java
@@ -1,31 +1,31 @@
 package edu.hm.peither_bartolov.a05_decoratorpattern.base;
 
 import edu.hm.peither_bartolov.a05_decoratorpattern.Counter;
-import java.util.Arrays;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 
 public class LoopCounter implements Counter{
-    private final List<Integer> list;
+    private final int[] list;
     
     private int currentPosition = 0;
     
-    public LoopCounter(Integer... args){
+    public LoopCounter(int... args){
         if(args.length == 0)
             throw new NoSuchElementException();
-        list = Arrays.asList(args);
+        
+        list = new int[args.length];
+        System.arraycopy(args,0, list,0, args.length);
     }
     
     
     @Override 
     public int read() {
-        return list.get(currentPosition);
+        return list[currentPosition];
     }
 
     @Override 
     public LoopCounter tick() {
-        currentPosition = (currentPosition+1)%list.size();
+        currentPosition = (currentPosition+1)%list.length;
         return this;
     }
 }
