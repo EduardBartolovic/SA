7092cf1de8425261d9100733001f89cf5eb4e807
Felix Peither <felix@peither.de>
2018-04-24 14:53:28 +0200

    nary counter

--- SA/src/edu/hm/peither_bartolov/a05_decoratorpattern/base/NaryCounter.java
+++ SA/src/edu/hm/peither_bartolov/a05_decoratorpattern/base/NaryCounter.java
@@ -1,17 +1,54 @@
 package edu.hm.peither_bartolov.a05_decoratorpattern.base;
 
+
 import edu.hm.peither_bartolov.a05_decoratorpattern.Counter;
 
 public class NaryCounter implements Counter{
 
+    private final int numberSystem;
+    
+    private int currentValue = 0;
+    
+    public NaryCounter(int numberSystem) {
+        this.numberSystem =  numberSystem;
+    }
+    
     @Override
     public int read() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return currentValue;
     }
 
     @Override
     public Counter tick() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        char[] numberPartsAsChars = Integer.toString(currentValue).toCharArray();
+        int[] numberParts = new int[numberPartsAsChars.length];
+        for (int index = 0; index < numberPartsAsChars.length; index++) {
+            final String value = "" + numberPartsAsChars[index];
+            numberParts[index] = Integer.parseInt(value);
+        }
+        numberParts[numberParts.length-1]++;
+        currentValue = 0;
+        int overflow = 0;
+        for (int index = numberParts.length-1; index >= 0; index--) {
+            if (overflow == 1) {
+                numberParts[index]++;
+                overflow = 0;
+            }
+            if (numberParts[index] == numberSystem) {
+                overflow = 1;
+                numberParts[index] = 0;
+            }
+            if (index == 0 && overflow == 1) {
+                currentValue = (int)Math.pow(10, numberParts.length);
+            }
+            
+        }
+        int powerTo = numberParts.length-1;
+        for (int index = 0; index < numberParts.length; index++) {
+            currentValue += (int)(Math.pow(10, powerTo) * numberParts[index]);
+            powerTo--;
+        }
+        return this;
     }
     
 }
