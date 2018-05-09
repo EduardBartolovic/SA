b6b2125db8d32f4b5974072158d71941f587db8c
Felix Peither <felix@peither.de>
2018-05-09 09:04:15 +0200

    CounterFactory Abstract rework

--- SA/src/edu/hm/bartolov/a06_factory/CounterFactory.java
+++ SA/src/edu/hm/bartolov/a06_factory/CounterFactory.java
@@ -2,7 +2,9 @@ package edu.hm.bartolov.a06_factory;
 
 import edu.hm.cs.rs.arch.a05_decorator.Counter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Abstract Factory class.
@@ -11,8 +13,17 @@ import java.util.List;
  */
 public abstract class CounterFactory {
     
-    private static final List<CounterFactory> myFactories = new ArrayList<>();
+//    private static final List<CounterFactory> myFactories = new ArrayList<>();
     
+    /**
+     * 
+     */
+    private static final Map<String, CounterFactory> myFactoriesMap = new HashMap<>();
+    
+    /**
+     * 
+     * @return 
+     */
     public static CounterFactory get(){
         
         final String type = System.getProperty("Factory.type");
@@ -24,8 +35,21 @@ public abstract class CounterFactory {
         return getMyFactory(addCounterFactory(type));
     }
     
+    /**
+     * 
+     * @param factoryType
+     * @return 
+     */
     private static CounterFactory getMyFactory(String factoryType) {
         CounterFactory myFactory;
+        
+//        if (myFactoriesMap.keySet().contains(factoryType)) {
+//            myFactory = myFactoriesMap.get(factoryType);
+//        }
+
+        if (myFactoriesMap.keySet().contains(factoryType)) {
+            return myFactoriesMap.get(factoryType);
+        }
         
         switch (factoryType) {
             case "FakeCounterFactory":
@@ -41,16 +65,16 @@ public abstract class CounterFactory {
                 throw new IllegalArgumentException("Please enter a valid Factory type.");
         }
         
-        
-        if (myFactories.contains(myFactory)) {
-            myFactory = myFactories.get(myFactories.indexOf(myFactory));
-        } else {
-            myFactories.add(myFactory);
-        }
+        myFactoriesMap.put(factoryType, myFactory);
         
         return myFactory;
     }
     
+    /**
+     * 
+     * @param typename
+     * @return 
+     */
     private static String addCounterFactory(String typename){
         if(typename.contains("CounterFactory"))
             return typename;
@@ -64,10 +88,9 @@ public abstract class CounterFactory {
      * @param string string
      * @param args varargs
      * @return fakeCounter
+     * @throws java.lang.ReflectiveOperationException
      */
-    public Counter make(String string,int... args) throws ReflectiveOperationException {
-        return null;
-    }
+    public abstract Counter make(String string,int... args) throws ReflectiveOperationException;
     
     /**
      * producing Counter.
@@ -75,9 +98,8 @@ public abstract class CounterFactory {
      * @param string
      * @param arg
      * @return fakeCounter
+     * @throws java.lang.ReflectiveOperationException
      */
-    public Counter make(Counter counter,String string,int arg) throws ReflectiveOperationException {
-        return null;
-    }
+    public abstract Counter make(Counter counter,String string,int arg) throws ReflectiveOperationException;
     
 }
