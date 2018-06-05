6360e543d013d7fadc1c0317217c2f0c28e2aef2
EduardBartolovic <zxeduardxz@gmail.com>
2018-06-05 13:44:36 +0200

    type correction

--- SA/src/edu/hm/bartolov/a08_mvc/datastore/readonly/Offerings.java
+++ SA/src/edu/hm/bartolov/a08_mvc/datastore/readonly/Offerings.java
@@ -1,12 +1,11 @@
 package edu.hm.bartolov.a08_mvc.datastore.readonly;
 
-import edu.hm.bartolov.a08_mvc.datastore.writeable.MutableArtwork;
 import java.util.stream.Stream;
 
 
 public interface Offerings {
     
-    Stream<MutableArtwork> getArtworks();
+    Stream<? extends Artwork> getArtworks();
 
     int getStepsRemaining();
 
