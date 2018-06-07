2286cb6ee072bd7bd2cacd393d8427d8e47fcebd
EduardBartolovic <zxeduardxz@gmail.com>
2018-06-07 17:57:35 +0200

    tests
    some bug fixes

--- SA/src/edu/hm/bartolov/a08_mvc/view/Logger.java
+++ SA/src/edu/hm/bartolov/a08_mvc/view/Logger.java
@@ -1,6 +1,7 @@
 package edu.hm.bartolov.a08_mvc.view;
 
 import edu.hm.bartolov.a08_mvc.datastore.readonly.Artwork;
+import edu.hm.bartolov.a08_mvc.datastore.readonly.Offerings;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
@@ -27,48 +28,8 @@ public class Logger extends Viewer{
     
     private int updates = 0;
     
-    Logger(Object... args) {
-        super(null);
-        
-//        BID.put(0, (data, bw) ->  {try {
-//                                    bw.write(data[0]+": "+data[1]+" zum Dritten, verkauft!");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        BID.put(1, (data, bw) ->  {try {
-//                                    bw.write(data[0]+": "+data[1]+" zum Zweiten...");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        BID.put(2, (data, bw) ->  {try {
-//                                    bw.write(data[0]+": "+data[1]+" zum Ersten...");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        BID.put(3, (data, bw) ->  {try {
-//                                    bw.write(data[0] + ": noch " +data[1]+ " geboten, hoere ich mehr?");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        BID.put(4, (data, bw) ->  {try {
-//                                    bw.write(data[0]+": "+data[1]+" geboten, hoere ich mehr?");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        BID.put(5, (data, bw) ->  {try {
-//                                    bw.write(data[0]+": "+data[1]+" geboten!");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        
-//        
-//        NOBID.put(0, (data, bw) -> {try {
-//                                    bw.write(data[0]+": " + "Keine Gebote, nicht verkauft.");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        NOBID.put(1, (data, bw) -> {try {
-//                                    bw.write(data[0]+": "+data[1]+" zum Zweiten...");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        NOBID.put(2, (data, bw) -> {try {
-//                                    bw.write(data[0]+": "+data[1]+" zum Ersten...");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        NOBID.put(3, (data, bw) -> {try {
-//                                    bw.write(data[0] + ": Mindestgebot noch " +data[1]+ " , bietet jemand?");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        NOBID.put(4, (data, bw) -> {try {
-//                                    bw.write(data[0]+": Mindestgebot "+data[1]+" , bietet jemand?");
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-//        NOBID.put(5, (data, bw) -> {try {
-//                                    bw.write(data[0]+": Mindestgebot "+data[1]);
-//                                    } catch (IOException ex ) {System.out.println(ERROR);}});
-        
+    Logger(Object arg) {
+        super((Offerings)arg);
     }
 
     @Override
@@ -80,23 +41,15 @@ public class Logger extends Viewer{
         
         try (BufferedWriter bw = Files.newBufferedWriter(file.toPath(), Charset.defaultCharset())) {
             
-            getDataStore()
-                    .getArtworks()
-                    .peek((Artwork art)->{
-                        try {
-                            bw.write("Title: "+art.getTitle()+" Sold: "+art.isAuctioned()+" InitialPrice: "+art.getInitialPrice()+" Buyer: "+art.getBuyer()+" SoldPrice: "+art.getSoldPrice());
-                        } catch (IOException ex ) {
-                            System.out.println(ERROR);
-                        }})
-                    .count();
-
-//            if(getDataStore().getBidder()==null){  
-//                NOBID.get(getDataStore().getStepsRemaining())
-//                        .accept(new String[]{artwork.getTitle(),Integer.toString(artwork.getInitialPrice())}, bw);
-//            }else{
-//                BID.get(getDataStore().getStepsRemaining())
-//                        .accept(new String[]{artwork.getTitle(),Integer.toString(getDataStore().getBid())}, bw);
-//            }
+            final long count = super.getDataStore()
+                                        .getArtworks()
+                                        .peek((Artwork art)->{
+                                            try {
+                                                bw.write("Title: "+art.getTitle()+" Sold: "+art.isAuctioned()+" InitialPrice: "+art.getInitialPrice()+" Buyer: "+art.getBuyer()+" SoldPrice: "+art.getSoldPrice());
+                                            } catch (IOException ex ) {
+                                                System.out.println(ERROR);
+                                            }})
+                                        .count();
             
         } catch (IOException ex) {
             System.out.println(ERROR);
