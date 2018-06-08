2c2eec682ca06c0d628072a0fcc7c4051390e743
Felix Peither <felix@peither.de>
2018-06-08 14:31:55 +0200

    changes

--- SA/src/edu/hm/bartolov/a08_mvc/Main.java
+++ SA/src/edu/hm/bartolov/a08_mvc/Main.java
@@ -1,5 +1,9 @@
 package edu.hm.bartolov.a08_mvc;
 
+import edu.hm.bartolov.a08_mvc.datastore.writeable.MutableArtwork;
+import edu.hm.bartolov.a08_mvc.datastore.writeable.MutableOfferings;
+import edu.hm.bartolov.a08_mvc.view.Viewer;
+
 /**
  *
  * @author Edo
@@ -7,6 +11,16 @@ package edu.hm.bartolov.a08_mvc;
 public class Main {
     
     public static void main(String... args){
+        MutableArtwork aw1 = MutableArtwork.make("TEST1", 20);
+        MutableArtwork aw2 = MutableArtwork.make("TEST2", 21);
+        
+        MutableOfferings offer = MutableOfferings.make(aw1, aw2);
+    
+        Viewer spectator = Viewer.make("spectator", offer);
+        offer.setBid(55);
+        offer.setBidder("hallo");
+        offer.notifyObservers();
+        spectator.update(offer, new Object());
         
     }
     
