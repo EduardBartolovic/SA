121f1f08a16ce6174cb4e75b6e994d86594d0635
Felix Peither <felix@peither.de>
2018-06-14 14:50:56 +0200

    Console and Robot controller funktionieren

--- SA/src/edu/hm/bartolov/a08_mvc/control/ConsoleController.java
+++ SA/src/edu/hm/bartolov/a08_mvc/control/ConsoleController.java
@@ -23,7 +23,12 @@ public class ConsoleController extends Controller{
     /**
      * The bytecode for a new line.
      */
-    private static final byte[] EMPTY_INPUT = new byte[]{10};
+    private static final byte[] NEW_LINE_BYTE = new byte[]{10};
+    
+    /**
+     * The bytecode for a empty byte.
+     */
+    private static final byte[] EMPTY_BYTE = new byte[1];
     
     /**
      * Constant to shift the bid.
@@ -47,25 +52,25 @@ public class ConsoleController extends Controller{
     public void run() {
         
         boolean auctionRunning = true;
-        int bid = 0;
         byte[] inputBytes = new byte[LINE_LENGTH];
         
         while (auctionRunning) {
+            int bid = 0;
             try {
                 System.in.read(inputBytes);
             } catch (IOException ioe) {
                 
             }
-            if (inputBytes[0] == EMPTY_INPUT[0])
+            if (inputBytes[0] == NEW_LINE_BYTE[0])
                 auctionRunning = false;
             
             for (int counter = 0; counter < LINE_LENGTH; counter++) {
                 char number = (char) inputBytes[counter];
-                if (Character.isDigit(number)) {//((int)inputCharacters[counter] - 48) > 0 &&((int) inputCharacters[counter] - 48) <= 9) {
+                if (Character.isDigit(number)) {
                     bid *= TIMES;  
                     bid += Integer.parseInt(Character.toString(number)); 
-                } else if (!Character.isDigit(number) &&(int) number != 10) {
-                    throw new IllegalArgumentException();
+                } else if (!Character.isDigit(number) && (int) number != NEW_LINE_BYTE[0] && number != EMPTY_BYTE[0]) {
+                    throw new IllegalArgumentException("this is not a number: " + number);
                 }
             }
             auctioneer.placebid(CONSOLE_CONTROLLER, bid);
